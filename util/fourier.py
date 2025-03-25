ed5468b7abc49ba10068fba43545a244a7ab11b7
Aditya Pratap Singh <adi18jan1999@gmail.com>
2025-03-25 11:31:32 -0400

    bug fix - typos

--- util/fourier.py
+++ util/fourier.py
@@ -20,21 +20,30 @@ def fft_per_channel(image):
 
     return fft_shifted
 
-def ifft_per_channel(fft_shifted):
+def ifft_per_channel(fft_shifted, get_img=False):
     """
     Computes the 2D inverse FFT of a shifted FFT image, channel by channel,
     using PyTorch, and applies ifftshift.
 
     Args:
         fft_shifted (torch.Tensor): Complex-valued FFT of the image, shape (C, H, W) or (B, C, H, W), shifted.
+        get_img (bool): If set to True, returns a ready-to-plot batch of images (uint8, 0-255).
 
     Returns:
         torch.Tensor: Real-valued inverse FFT of the image, shape (C, H, W) or (B, C, H, W).
+                    If get_img is True, returns a uint8 tensor representing the image(s).
     """
     if fft_shifted.ndim == 3 or fft_shifted.ndim == 4:
         ifft_image = torch.fft.ifftshift(fft_shifted, dim=(-2, -1))
         ifft_result = torch.fft.ifftn(ifft_image, dim=(-2, -1))
     else:
         raise ValueError("Input fft image must have 3 or 4 dimensions (C, H, W) or (B, C, H, W).")
+
+    if get_img:
+        if fft_shifted.ndim == 3
+            ifft_result = ifft_result.permute(1, 2, 0)
+        elif fft_shifted.ndim == 4:
+            ifft_result = ifft_result.permute(0, 2, 3, 1)
+        return (ifft_result * 255.0).to(torch.uint8)
 
     return ifft_result
